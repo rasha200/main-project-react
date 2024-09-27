33baa7bdc5acdfb2d3c8d082cf89eb5556f7286a
kholoudjuhany <juhanykholoud1@gmail.com>
2024-09-28 00:01:27 +0300

    CRUDS

--- backend/app/Models/ContactUs.php
+++ backend/app/Models/ContactUs.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class ContactUs extends Model
 {
     use HasFactory;
+    protected $fillable = ['contact_subject', 'contact_message', 'user_id'];
+
+    public function user(){
+        return $this->belongsTo(User::class,'user_id');
+    }
+
 }
