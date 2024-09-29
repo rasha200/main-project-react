a74bf24c26a3f12deb57596f75e13bfd117bba61
RAGHAD-HISHAM <raghadstudies2020@gmail.com>
2024-09-29 14:38:34 +0300

    add  backend/routes/api.php

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -12,7 +12,7 @@ use App\Http\Controllers\StudentController;
 Route::get('courses', [CourseController::class, 'index']);
 Route::post('courses', [CourseController::class, 'store']);
 Route::get('courses/{id}', [CourseController::class, 'show']);
-Route::put('courses/{id}', [CourseController::class, 'update']);
+Route::put('courses/{course}', [CourseController::class, 'update']);
 Route::delete('courses/{id}', [CourseController::class, 'destroy']);
 
 // Assign students to a course
